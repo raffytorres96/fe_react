0658d989b45ca4e0ec7b20de9b8fe48c7e1cf61d
Raffaele Gatta <163126175+raffytorres96@users.noreply.github.com>
2026-06-11 18:00:36 +0200

    creazione componente utente

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,22 @@ import Biglietto from './components/Biglietto.jsx'
 import Meteo from './components/Meteo.jsx'
 import Card from './components/Card/Card.jsx'
 import Card2 from './components/Card2/Card2.jsx'
+import Utente from './components/Utente/Utente.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>PRIMO REACT</h1>
-      <Biglietto nome="Giacomo" destinazione="Roma"/>
+      <Utente />
+      {/* <Biglietto nome="Giacomo" destinazione="Roma"/>
       <Biglietto nome="Emma" destinazione="Bari"/>
       <Biglietto nome="Marta" destinazione="Como"/>
       <Biglietto destinazione="Marrachesh"/>
       <h2>METEO INFO</h2>
       <Meteo />
       <Card />
-      <Card2 />
+      <Card2 /> */}
     </>
   );
 }
